68e4ddce5a11f912db58ea820fcf5cf271b472df
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2025-02-13 00:44:42 +0000

    added: New Functions.

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,38 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaugusto <aaugusto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/09 16:17:47 by aaugusto          #+#    #+#             */
-/*   Updated: 2025/02/13 00:44:14 by aaugusto         ###   ########.fr       */
+/*   Created: 2025/02/12 17:32:48 by aaugusto          #+#    #+#             */
+/*   Updated: 2025/02/12 23:51:00 by aaugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
+	unsigned char		*d;
+	const unsigned char	*s;
 	size_t	i;
 
-	if (size == 0)
-		return ((size_t)ft_strlen(src));
-	while(i < (size - 1) && src[i])
+	if(!dest && !src)
+		return (dest);
+	if (dest < src)
+		return (ft_memcpy(dest, src, n));
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	while (i < n)
 	{
-		dest[i] = src[i];
+		d[i] = s[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return ((size_t)ft_strlen(src));
+	return (dest);	
 }
-/* 
+
 int	main(void)
 {
-	char dest[50];
-	char src[] = "Function String Copy";
-	
-	printf("Function Return: %zu\n", ft_strlcpy(dest, src, 21));
-	printf("Dest Value: %s\n", dest);
+	char str[] = "Original String";
+	char dest[16];
+	char *result;
+
+	result = ft_memmove(dest, str, 16);
+
+	printf("Ft_memmove: %s\n", result);
 	return (0);
-} */
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aaugusto <aaugusto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 00:34:10 by aaugusto          #+#    #+#             */
-/*   Updated: 2025/02/11 12:37:20 by aaugusto         ###   ########.fr       */
+/*   Updated: 2025/02/13 00:39:05 by aaugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,10 @@ int	ft_isascii(int c);
 int ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t len);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
